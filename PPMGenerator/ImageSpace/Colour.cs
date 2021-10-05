4ab91e0f2e2eb998e263f63f5ef00efd5f4ef0b5
Nicolas <nicolusmaluleke@gmail.com>
2021-10-05 23:06:10 +0200

    Created Color to represent a Pixel;

---
+++ PPMGenerator/ImageSpace/Colour.cs
@@ -0,0 +1,18 @@
+using System.Threading.Tasks;
+
+namespace PPMGenerator.ImageSpace
+{
+    public struct Colour
+    {
+        public int Red;
+        public int Green;
+        public int Blue;
+
+        public Colour(int red, int green, int blue)
+        {
+            Red = red;
+            Green = green;
+            Blue = blue;
+        }
+    }
+}
