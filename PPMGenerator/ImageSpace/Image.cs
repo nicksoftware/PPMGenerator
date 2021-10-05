c807396480f0d982aa652d094e7cf097ca13e7a8
Nicolas <nicolusmaluleke@gmail.com>
2021-10-05 23:06:24 +0200

    Created an Image.

---
+++ PPMGenerator/ImageSpace/Image.cs
@@ -0,0 +1,49 @@
+using System;
+using System.IO;
+using System.Text;
+
+namespace PPMGenerator.ImageSpace
+{
+    public class Image
+    {
+        private readonly int _rows;
+        private readonly int _cols;
+        private readonly Colour[,] _pixels;
+
+        public Image(int cols, int rows)
+        {
+            _cols = cols;
+            _rows = rows;
+            _pixels = new Colour[_rows,_cols];
+        }
+        public void SetPixel(int row, int col, Colour pixel) => _pixels[row, col] = pixel;
+
+        public Colour GetPixel(int row, int col) => _pixels[row, col];
+
+        public void  ToPpmImage()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("P3");
+            sb.AppendLine($"{_cols} {_rows}");
+            sb.AppendLine("255"); //Highest Color intensity
+
+            string space = " ";
+            for (int r = 0; r < _rows; r++)
+            {
+                for (int c = 0; c < _cols; c++)
+                {
+                    var pixel = GetPixel(r, c);
+                    sb.Append(pixel.Red);
+                    sb.Append(space);
+                    sb.Append(pixel.Green);
+                    sb.Append(space);
+                    sb.Append(pixel.Blue);
+                    sb.AppendLine();
+                }
+            }
+            string text = sb.ToString();
+            string fileName = Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "image.ppm");
+            File.WriteAllText(fileName,text);
+        }
+    }
+}
