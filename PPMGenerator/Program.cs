23698d4a70ec411f4bef860da86eaacc00dd7f19
Nicolas <nicolusmaluleke@gmail.com>
2021-10-05 23:07:12 +0200

    Added Program Entry File.

---
+++ PPMGenerator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using PPMGenerator.ImageSpace;
+
+namespace PPMGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = 400;
+            int cols = 200;
+            var image = new Image(cols, rows);
+            var rollDice = new Random();
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < cols; c++)
+                {
+                    Colour pixel = new Colour(rollDice.Next(0,255), rollDice.Next(0,255), rollDice.Next(0,255));
+                    image.SetPixel(r, c, pixel);
+                }
+            }
+
+            image.ToPpmImage();
+        }
+    }
+}
