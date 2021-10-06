31549c4938d456d4870a65ef549dace373556e00
Nicolas <nicolusmaluleke@gmail.com>
2021-10-06 07:23:41 +0200

    top level entry point.

--- PPMGenerator/Program.cs
+++ PPMGenerator/Program.cs
@@ -1,31 +1,19 @@
 ﻿using System;
-using System.IO;
-using System.Threading.Tasks;
 using PPMGenerator.ImageSpace;
-
-namespace PPMGenerator
+int rows = 400;
+int cols = 200;
+var image = new Image(cols, rows);
+var rollDice = new Random();
+for (int row = 0; row < rows; row++)
 {
-    class Program
+    for (int col = 0; col < cols; col++)
     {
-        static void Main(string[] args)
-        {
-            int rows = 400;
-            int cols = 200;
-            var image = new Image(cols, rows);
-            var rollDice = new Random();
-            for (int row = 0; row < rows; row++)
-            {
-                for (int col = 0; col < cols; col++)
-                {
-                    var red = rollDice.Next(0, 255);
-                    var green = rollDice.Next(0, 255);
-                    var blue = rollDice.Next(0, 255);
-                    Colour pixel = new Colour(red, green,blue);
-                    image.SetPixel(row, col, pixel);
-                }
-            }
-            var filePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-            image.ToPpmImage(filePath);
-        }
+        var red = rollDice.Next(0, 255);
+        var green = rollDice.Next(0, 255);
+        var blue = rollDice.Next(0, 255);
+        Colour pixel = new Colour(red, green,blue);
+        image.SetPixel(row, col, pixel);
     }
 }
+var filePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+image.ToPpmImage(filePath);
