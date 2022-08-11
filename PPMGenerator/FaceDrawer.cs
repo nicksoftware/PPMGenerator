958de1374c8fdac9d3ade2bfc25ad30ecb36c6b9
Nicolas <nicolusmaluleke@gmail.com>
2022-08-11 05:19:45 +0200

    Added color fill.

--- PPMGenerator/FaceDrawer.cs
+++ PPMGenerator/FaceDrawer.cs
@@ -2,168 +2,206 @@ using System;
 using PPMGenerator.ImageSpace;
 using static System.Console;
 
-namespace PPMGenerator;
-
-/**
- * FaceDrawer class
- * 
- * @author Nicolas Maluleke 
- * @version 1.0
- */
-
-public class FaceDrawer
+namespace PPMGenerator
 {
-    private readonly int rows;
-    private readonly int cols;
-    private readonly Image image;
-    private readonly int area = int.MinValue;
-    private readonly double radius = double.MinValue;
-
     /**
-        * FaceDrawer constructor
+     * FaceDrawer class
+     * 
+     * @author Nicolas Maluleke 
+     * @version 1.0
+     */
+
+    public class FaceDrawer
+    {
+        private readonly int rows;
+        private readonly int cols;
+        private readonly Image image;
+        private readonly int area = int.MinValue;
+        private readonly double radius = double.MinValue;
+
+        /**
+            * FaceDrawer constructor
+            *
+            * @param rows number of pixels in the image horizontally
+            * @param cols number of pixels in the image vertically
+            */
+
+        public FaceDrawer(int rows = 600, int cols = 600)
+        {
+            this.rows = rows;
+            this.cols = cols;
+            area = rows * cols;
+            radius = Math.Sqrt(area / Math.PI);
+            image = new(cols, rows);
+        }
+
+        public FaceDrawer WithFace(Color color)
+        {
+            WriteLine("Drawing Face Circle");
+            DrawCircle(rows / 2, cols / 2, (int)(radius / 2), color);
+            return this;
+        }
+
+        public FaceDrawer WithEye(Color color, Color pupilColor, Eye eye = Eye.Left)
+        {
+            int xPosition = (int)(rows * .36);
+            int yPosition = (int)(cols * .40);
+            int circleSize = 20;
+
+            if (eye == Eye.Right)
+            {
+                xPosition = rows - xPosition;
+            }
+
+            WriteLine($"Drawing {eye} Eye Circle");
+
+            while (circleSize >= 9)
+            {
+                DrawCircle(xPosition, yPosition, (int)radius / circleSize, color);
+                circleSize--;
+            }
+
+            circleSize = rows;
+            while (circleSize > rows * 0.05)
+            {
+                DrawCircle(xPosition, yPosition, (int)(radius / circleSize), pupilColor);
+                circleSize -= 1;
+            }
+
+            return this;
+        }
+
+        public FaceDrawer WithNose(Color color)
+        {
+            WriteLine("Drawing Nose Circle");
+            int circleSize = rows;
+            while (circleSize > rows * 0.045)
+            {
+                DrawCircle(rows / 2, cols / 2, (int)(radius / circleSize), color);
+                circleSize -= 1;
+            }
+
+            circleSize = rows;
+            while (circleSize >= rows * 0.111)
+            {
+                DrawCircle(rows / 2, cols / 2, (int)radius / circleSize, color);
+
+                DrawCircle((int)(rows / 1.94), (int)(cols / 1.91), (int)(radius / circleSize), color);
+                DrawCircle((int)(rows / 2.05), (int)(cols / 1.91), (int)(radius / circleSize), color);
+                circleSize--;
+            }
+
+            return this;
+        }
+
+        public FaceDrawer WithMouth(Color color, Mouth mouth = Mouth.Open)
+        {
+            int yPosition = (int)(cols * .66);
+            int xPosition = rows / 2;
+            if (mouth == Mouth.Open)
+            {
+                int circleSize = rows;
+                while (circleSize >= rows * 0.0175)
+                {
+                    DrawCircle(xPosition, yPosition, (int)(radius / circleSize), color);
+                    circleSize -= 1;
+                }
+                WriteLine("Drawing Open Mouth Circle");
+            }
+            else
+            {
+                WriteLine("Drawing Closed Mouth Circle");
+                DrawClosedMouth(xPosition, yPosition, (int)radius / 20, color);
+            }
+
+            return this;
+        }
+
+        public void ToPpmImage(string filePath, string fileName = "face")
+        {
+            WriteLine("Writing PPM Image");
+            image.ToPpmImage(filePath, fileName);
+            WriteLine("Done");
+        }
+
+        public FaceDrawer ClearCanvas(Color color)
+        {
+            WriteLine("Clearing Canvas...");
+            image.ClearCanvas(color);
+            WriteLine("Canvas Cleared...");
+            return this;
+        }
+
+        public Image ToImage()
+        {
+            WriteLine("Converting to Image...");
+            return image;
+        }
+
+        /**
+        * DrawCircle method
         *
-        * @param rows number of pixels in the image horizontally
-        * @param cols number of pixels in the image vertically
+        * @param x x-coordinate of the center of the circle
+        * @param y y-coordinate of the center of the circle
+        * @param radius radius of the circle
+        * @param color color of the circle
         */
-
-    public FaceDrawer(int rows = 600, int cols = 600)
-    {
-        this.rows = rows;
-        this.cols = cols;
-        area = rows * cols;
-        radius = Math.Sqrt(area / Math.PI);
-        image = new(cols, rows);
-    }
-
-    public FaceDrawer WithFace(Color color)
-    {
-        WriteLine("Drawing Face Circle");
-        DrawCircle(rows / 2, cols / 2, (int)radius / 2, color);
-        return this;
-    }
-
-    public FaceDrawer WithEye(Color color, Eye eye = Eye.Left)
-    {
-        int xPosition = (int)(rows * .36);
-        int yPosition = (int)(cols * .40);
-        if (eye == Eye.Right)
-            xPosition = rows - xPosition;
-
-        WriteLine($"Drawing {eye} Eye Circle");
-        DrawCircle(xPosition, yPosition, (int)radius / 9, color);
-
-        return this;
-    }
-
-    public FaceDrawer WithNose(Color color)
-    {
-        WriteLine("Drawing Nose Circle");
-        DrawCircle(rows / 2, cols / 2, (int)radius / 20, color);
-        return this;
-    }
-
-    public FaceDrawer WithMouth(Color color, Mouth mouth = Mouth.Open)
-
-    {
-        int yPosition = (int)(cols * .66);
-        if (mouth == Mouth.Open)
+        private void DrawCircle(int row, int col, int radius, Color color)
         {
-            WriteLine("Drawing Open Mouth Circle");
-            DrawCircle(rows / 2, yPosition, (int)radius / 7, color);
-        }
-        else
-        {
-            WriteLine("Drawing Closed Mouth Circle");
-            DrawClosedMouth(rows / 2, yPosition, (int)radius / 20, color);
-        }
+            const double PI = Math.PI;
+            double d, angle, x1, y1;
 
-        return this;
-    }
-
-    public void ToPpmImage(string filePath, string fileName = "face")
-    {
-        WriteLine("Writing PPM Image");
-        image.ToPpmImage(filePath, fileName);
-        WriteLine("Done");
-    }
-
-    public FaceDrawer ClearCanvas(Color color)
-    {
-        WriteLine("Clearing Canvas...");
-        image.ClearCanvas(color);
-        WriteLine("Canvas Cleared...");
-        return this;
-    }
-
-    public Image ToImage()
-    {
-        WriteLine("Converting to Image...");
-        return image;
-    }
-
-    /**
-    * DrawCircle method
-    *
-    * @param x x-coordinate of the center of the circle
-    * @param y y-coordinate of the center of the circle
-    * @param radius radius of the circle
-    * @param color color of the circle
-    */
-    private void DrawCircle(int row, int col, int radius, Color color)
-    {
-        const double PI = Math.PI;
-        double d, angle, x1, y1;
-
-        for (int i = row - radius; i <= row + radius; i++)
-        {
-            for (d = 0; d < 360; d += 0.1)
+            for (int i = row - radius; i <= row + radius; i++)
             {
-                angle = d;
-                x1 = radius * Math.Cos(angle * PI / 180);
-                y1 = radius * Math.Sin(angle * PI / 180);
-
-                int drawX = (int)(col + y1);
-                int drawY = (int)(row + x1);
-                if (drawX >= 0 && drawX < rows && drawY >= 0 && drawY < cols)
+                for (d = 0; d < 360; d += 0.1)
                 {
-                    image.SetPixel(drawX, drawY, color);
+                    angle = d;
+                    x1 = radius * Math.Cos(angle * PI / 180);
+                    y1 = radius * Math.Sin(angle * PI / 180);
+
+                    int drawX = (int)(col + y1);
+                    int drawY = (int)(row + x1);
+                    if (drawX >= 0 && drawX < rows && drawY >= 0 && drawY < cols)
+                    {
+                        image.SetPixel(drawX, drawY, color);
+                    }
+                }
+            }
+        }
+
+        public void DrawClosedMouth(int row, int col, int radius, Color color)
+        {
+            const double PI = Math.PI;
+            double d, angle, x1, y1;
+
+            for (int i = row - radius; i <= row + radius; i++)
+            {
+                for (d = 0; d < 360; d += 0.1)
+                {
+                    angle = d;
+                    x1 = radius * Math.Cos(angle * PI / 2);
+                    y1 = radius * Math.Sin(angle * PI / 30);
+
+                    int drawX = (int)(col + y1);
+                    int drawY = (int)(row + x1);
+                    if (drawX >= 0 && drawX < rows && drawY >= 0 && drawY < cols)
+                    {
+                        image.SetPixel(drawX, drawY, color);
+                    }
                 }
             }
         }
     }
 
-    public void DrawClosedMouth(int row, int col, int radius, Color color)
+    public enum Eye
     {
-        const double PI = Math.PI;
-        double d, angle, x1, y1;
-
-        for (int i = row - radius; i <= row + radius; i++)
-        {
-            for (d = 0; d < 360; d += 0.1)
-            {
-                angle = d;
-                x1 = radius * Math.Cos(angle * PI / 2);
-                y1 = radius * Math.Sin(angle * PI / 30);
-
-                int drawX = (int)(col + y1);
-                int drawY = (int)(row + x1);
-                if (drawX >= 0 && drawX < rows && drawY >= 0 && drawY < cols)
-                {
-                    image.SetPixel(drawX, drawY, color);
-                }
-            }
-        }
+        Left,
+        Right
     }
-}
 
-public enum Eye
-{
-    Left,
-    Right
-}
-
-public enum Mouth
-{
-    Open,
-    Closed
+    public enum Mouth
+    {
+        Open,
+        Closed
+    }
 }
--- PPMGenerator/Program.cs
+++ PPMGenerator/Program.cs
@@ -2,17 +2,21 @@
 using PPMGenerator;
 using PPMGenerator.ImageSpace;
 
-int imageWidth = 600;
-int imageHeight = 600;
+int imageWidth = 400;
+int imageHeight = 400;
 
-Color pencilColor = new(128, 128, 128);
+Color faceColor = new(128, 128, 128);
+Color eyeColor = new(99, 78, 52);
+Color pupilColor = new(50, 26, 24);
+Color noseColor = eyeColor;
+Color mouthColor = new(102, 51, 0);
 string filePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
 
 new FaceDrawer(imageWidth, imageHeight)
     .ClearCanvas(new Color(255, 255, 255))
-    .WithFace(pencilColor)
-    .WithEye(pencilColor, Eye.Left)
-    .WithEye(pencilColor, Eye.Right)
-    .WithNose(pencilColor)
-    .WithMouth(pencilColor, Mouth.Open)
+    .WithFace(faceColor)
+    .WithEye(eyeColor, pupilColor, Eye.Left)
+    .WithEye(eyeColor, pupilColor, Eye.Right)
+    .WithNose(noseColor)
+    .WithMouth(mouthColor, Mouth.Open)
     .ToPpmImage(filePath);
