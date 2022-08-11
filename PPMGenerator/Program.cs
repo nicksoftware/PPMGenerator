b49937addab8389409bf2729291cfb5c780d58b7
Nicolas <nicolusmaluleke@gmail.com>
2022-08-11 02:44:40 +0200

    Draw  Face made of Circles.

--- PPMGenerator/Program.cs
+++ PPMGenerator/Program.cs
@@ -1,21 +1,34 @@
 ﻿using System;
 using PPMGenerator.ImageSpace;
 
-var rows = 200;
-var cols = 200;
-var image = new Image(cols, rows);
-var rollDice = new Random();
+int rows = 600;
+int cols = 600;
+Image image = new(cols, rows);
 
-for (int row = 0; row < rows; row++)
-{
-    for (int col = 0; col < cols; col++)
-    {
-        var red = rollDice.Next(0, 255);
-        var green = rollDice.Next(0, 255);
-        var blue = rollDice.Next(0, 255);
-        Color pixel = new Color(red, green,blue);
-        image.SetPixel(row, col, pixel);
-    }
-}
-var filePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+int area = rows * cols;
+double radius = Math.Sqrt(area / Math.PI);
+Color pencilColor = new(128, 128, 128);
+
+image.ClearCanvas(new Color(255, 255, 255));
+
+System.Console.WriteLine("Drawing Face Circle");
+image.DrawCircle(rows / 2, cols / 2, (int)radius / 2, pencilColor);
+
+System.Console.WriteLine("Drawing Left Eye Circle");
+image.DrawCircle(220, 240, (int)radius / 8, pencilColor);
+
+
+System.Console.WriteLine("Drawing Left Eye Circle");
+image.DrawCircle(rows - 220, 240, (int)radius / 8, pencilColor);
+
+
+System.Console.WriteLine("Drawing Nose Circle");
+image.DrawCircle(300, cols / 2, (int)radius / 30, pencilColor);
+
+
+System.Console.WriteLine("Drawing mouth  Circle");
+image.DrawCircle(rows / 2, 400, (int)radius / 8, pencilColor);
+
+string filePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
 image.ToPpmImage(filePath);
